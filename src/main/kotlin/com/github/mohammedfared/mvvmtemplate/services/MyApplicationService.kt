91fd7c9cf98228ed69e4d5d7b94cb02072dc89d7
GitHub Action <action@github.com>
2021-01-16 19:59:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mohammedfared/mvvmtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mohammedfared.mvvmtemplate.services
+
+import com.github.mohammedfared.mvvmtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
